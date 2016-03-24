1e4461881c938c92e68c7f07ae81cb9a2f75b669
sundalei <sundalei2011@163.com>
2016-03-24 18:11:24 +0800

    array

--- arrayTest.js
+++ arrayTest.js
@@ -22,8 +22,37 @@
 //alert(arr.indexOf(30));
 //alert(arr.indexOf('30'));
 
-var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-alert(arr.slice(0, 3));
-alert(arr.slice(3));
-var aCopy = arr.slice();
-alert(arr === aCopy);
+//var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+//alert(arr.slice(0, 3));
+//alert(arr.slice(3));
+//var aCopy = arr.slice();
+//alert(arr === aCopy);
+
+//var arr = ['Microsoft', 'Apple', 'Yahoo', 'AOL', 'Excite', 'Oracle'];
+//arr.splice(2, 3, "Google", "Facebook");
+//alert(arr);
+//arr.splice(2, 2);
+//alert(arr);
+//arr.splice(2, 0, 'Google', 'Facebook');
+//alert(arr);
+
+//var arr = ['A', 'B', 'C'];
+//var added = arr.concat(1, 2, [3, 4, 5], ['Array', 'Bob']);
+//alert(added);
+//alert(added.join('-'));
+
+'use strict';
+var arr = ['abc', 'cde', 'hello', 'world'];
+arr.sort();
+var message = "welcome ";
+for(var i = 0; i < arr.length; i++) {
+    if(i === arr.length - 1) {
+        message = message.concat('and ', arr[i]);
+    } else if(i === arr.length - 2) {
+        message = message.concat(arr[i], ' ');
+    } else {
+        message = message.concat(arr[i], ', ');
+    }
+}
+message = message.concat(" boys!");
+alert(message);
