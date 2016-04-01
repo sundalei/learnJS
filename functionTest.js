02d79baccc9fc474ba4b6ab8220619789495f704
sundalei <sundalei2011@163.com>
2016-04-01 18:01:25 +0800

    201604011801

--- functionTest.js
+++ functionTest.js
@@ -7,13 +7,7 @@ function foo(x) {
 
 //foo(10, 20, 30);
 
-function foo(a, b) {
-    var i, rest = [];
-    if(arguments.length > 2) {
-        for(i = 2; i < arguments.length; i++) {
-            rest.push(arguments[i]);
-        }
-    }
+function foo(a, b, ...rest) {
     console.log('a = ' + a);
     console.log('b = ' + b);
     console.log(rest);
---
+++ variableTest.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/*
+function foo() {
+    var y = 'Bob';
+    var x = 'Hello, ' + y;
+    alert(x);
+    //var y = 'Bob';
+}
+
+foo();
+*/
+
+/*
+var course = "Learn Javascript";
+alert(course);
+alert(window.course);
+*/
+
+function foo() {
+    alert('foo');
+}
+
+foo();
+window.foo();
