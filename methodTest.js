ee735932079eec471db008e09d93eae8c5788a6d
sundalei <sundalei2011@163.com>
2016-04-05 14:25:18 +0800

    201604051425

---
+++ methodTest.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/*
+var xiaoming = {
+    name: '小明',
+    birth: 1990,
+    age : function() {
+        var y = new Date().getFullYear();
+        return y - this.birth;
+    }
+};
+
+alert(xiaoming.age);
+alert(xiaoming.age());
+*/
+
+/*
+function getAge() {
+    var y = new Date().getFullYear();
+    return y - this.birth;
+}
+
+var xiaoming = {
+   name: '小明',
+    birth: 1990,
+    age : getAge
+};
+
+alert(xiaoming.age());
+alert(getAge.apply(xiaoming, []));
+*/
+
+/*
+var xiaoming = {
+    name: '小明',
+    birth: 1990,
+    age : function() {
+        var that = this;
+        function getAgeFromBirth() {
+            var y = new Date().getFullYear();
+            return y - that.birth;
+        }
+        return getAgeFromBirth();
+    }
+};
+
+alert(xiaoming.age());
+*/
+
+var count = 0;
+var oldParseInt = parseInt;
+
+window.parseInt = function() {
+    count += 1;
+    return oldParseInt.apply(null, arguments);
+};
+
+alert(parseInt('10'));
+alert(parseInt('20'));
+alert(parseInt('30'));
+alert(count);
--- variableTest.js
+++ variableTest.js
@@ -17,9 +17,38 @@ alert(course);
 alert(window.course);
 */
 
+/*
 function foo() {
     alert('foo');
 }
 
 foo();
 window.foo();
+*/
+
+/*
+window.alert('调用window.alert()');
+var old_alert = window.alert;
+window.alert = function() {}
+alert('无法用alert()显示了!');
+window.alert = old_alert;
+alert('又可以用alert()了!');
+*/
+
+/*
+var MYAPP = {};
+MYAPP.name = "myapp";
+MYAPP.version = "1.0";
+
+MYAPP.foo = function() {
+    return foo;
+};
+*/
+
+function foo() {
+    const PI = 3.14;
+    PI = 3;
+    alert(PI);
+}
+
+foo();
