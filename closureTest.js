e2e3e41d3bdf55199db2a8bd77324eaeafad13d7
sundalei <sundalei2011@163.com>
2016-04-07 23:38:25 +0800

    201604072338

--- closureTest.js
+++ closureTest.js
@@ -1,3 +1,4 @@
+/*
 function lazy_sum(arr) {
     var sum = function() {
         return arr.reduce(function(x, y) {
@@ -11,3 +12,46 @@ var result = f1();
 alert(result);
 var f2 = lazy_sum([1, 2, 3, 4, 5]);
 alert(f1 === f2);
+*/
+
+/*
+function count() {
+    var arr = [];
+    for(var i = 1; i <= 3; i++) {
+       arr.push((function(n) {
+           return function() {
+               return n*n;
+           }
+       })(i));
+    }
+    return arr;
+}
+
+var result = count();
+var f1 = result[0];
+var f2 = result[1];
+var f3 = result[2];
+alert(f1());
+alert(f2());
+alert(f3());
+*/
+
+'use strict';
+
+function create_counter(initial) {
+    var x = initial || 0;
+    return {
+        inc : function() {
+            x += 1;
+            return x;
+        }
+    }
+}
+var c1 = create_counter();
+alert(c1.inc());
+alert(c1.inc());
+alert(c1.inc());
+var c2 = create_counter(10);
+alert(c2.inc());
+alert(c2.inc());
+alert(c2.inc());
