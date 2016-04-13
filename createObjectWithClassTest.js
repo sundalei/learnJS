6522e783758f8ed653a5220d6e12e84a13e89e52
sundalei <sundalei2011@163.com>
2016-04-13 23:05:56 +0800

    201604132305

--- createObjectWithClassTest.js
+++ createObjectWithClassTest.js
@@ -9,5 +9,20 @@ class Student {
     }
 }
 
+class PrimaryStudent extends Student {
+    constructor(name, grade) {
+        super(name);
+        this.grade = grade;
+    }
+    
+    myGrade() {
+        alert('I am at grade ' + this.grade);
+    }
+}
+
 var xiaoming = new Student('xiaoming');
 xiaoming.hello();
+
+var xiaohong = new PrimaryStudent('xiaohong', 2);
+xiaohong.hello();
+xiaohong.myGrade();
