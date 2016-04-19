ab3957bd5510dfbb418187f3c34a30c398dba908
sundalei <sundalei2011@163.com>
2016-04-19 14:10:22 +0800

    201604191410

--- fileOperateTest.js
+++ fileOperateTest.js
@@ -1,10 +1,32 @@
-function checkForm() {
-    var f = document.getElementById('test-image-file');
-    var filename = f.value;
-    alert(filename);
+window.onload = function() {
+    var fileInput = document.getElementById('test-image-file');
+    var info = document.getElementById('test-file-info');
+    var preview = document.getElementById('test-image-preview');
     
-    if(!filename || !(filename.endsWith('jpg') || filename.endsWith('png') || filename.endsWith('gif'))) {
-        alert('Can only upload image file.');
-        return false;
+    fileInput.addEventListener('change', function() {
+        preview.style.backgroundImage = '';
+        
+        if(!fileInput.value) {
+            info.innerHTML = 'No File Selected.';
+            return;
+        }
+        
+        var file = fileInput.files[0];
+        info.innerHTML = 'file: ' + file.name + '<br>' +
+                     'size: ' + file.size + '<br>' +
+                     'update: ' + file.lastModifiedDate;
+    if (file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/gif') {
+        alert('No valid image file!');
+        return;
     }
+    
+     var reader = new FileReader();
+        reader.onload = function (e) {
+            var data = e.target.result;// data:image/jpeg;base64,
+            alert(data);
+            preview.style.backgroundImage = 'url("'+data+'")';
+        }
+        
+        reader.readAsDataURL(file);
+    });
 }
