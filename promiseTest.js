6e81e7e7c796c08d098ff463eeb688729371bbe9
sundalei <sundalei2011@163.com>
2016-04-20 09:41:03 -0400

    201604201341

---
+++ promiseTest.js
@@ -0,0 +1,56 @@
+/*
+function callback() {
+    console.log('Done');
+}
+console.log('before setTimeout()');
+setTimeout(callback, 1000);
+console.log('after setTimeout()');
+*/
+
+/*
+function test(resolve, reject) {
+    var timeOut = Math.random() * 2;
+    log('set timeout to: ' + timeOut + ' seconds.');
+    setTimeout(function() {
+        if(timeOut < 1) {
+            log('call resolve()...');
+            resolve('200 OK');
+        } else {
+            log('call reject()...');
+            reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+}
+*/
+
+var perform = function() {
+    var logging = document.getElementById('test-promise-log');
+    while(logging.children.length > 1) {
+        logging.removeChild(logging.children[logging.children.length - 1]);
+    }
+    
+    function log(s) {
+        var p = document.createElement('p');
+        p.innerHTML = s;
+        logging.appendChild(p);
+    }
+    
+    new Promise(function (resolve, reject) {
+        log('start new Promise...');
+        var timeOut = Math.random() * 2;
+        log('set timeout to: ' + timeOut + ' seconds.');
+        setTimeout(function() {
+        if(timeOut < 1) {
+            log('call resolve()...');
+            resolve('200 OK');
+        } else {
+            log('call reject()...');
+            reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+    }).then(function(r) {
+        log('Done: ' + r);
+    }).catch(function(reason) {
+        log('Failed: ' + reason);
+    });
+}
