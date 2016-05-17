721b7e29df552889613da4907d09c67e33377f1c
sundalei <sundalei2011@163.com>
2016-05-17 20:46:43 +0800

    201605172046

--- function/enclosure.js
+++ function/enclosure.js
@@ -56,3 +56,39 @@ var f3 = result[2];
 f1();
 f2();
 f3();
+
+/*************************************************/
+
+function create_counter(initial) {
+  var x = initial || 0;
+  return {
+    inc: function() {
+      x += 1;
+      return x;
+    }
+  }
+}
+
+var c1 = create_counter();
+c1.inc();
+c1.inc();
+c1.inc();
+
+var c2 = create_counter(10);
+c2.inc();
+c2.inc();
+c2.inc();
+
+/***************************************************/
+
+function make_pow(n) {
+  return function(x) {
+    return Math.pow(x, n);
+  }
+}
+
+var pow2 = make_pow(2);
+var pow3 = make_pow(3);
+
+pow2(5);
+pow3(7);
