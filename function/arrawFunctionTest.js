9396cb45b9079a0ca47e49928f9803035553e20f
sundalei <sundalei2011@163.com>
2016-05-18 10:58:03 +0800

    201605181057

---
+++ function/arrawFunctionTest.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var fn = x => {
+    if(x > 0) {
+        return x * x;
+    } else {
+        return -x * x;
+    }
+}
+fn(5);
+fn(-5);
+
+/*******************************************************************************/
+
+var fn = x => ({foo: x});
+fn(3);
+
+/*******************************************************************************/
+var obj = {
+    birth : 1990,
+    getAge : function() {
+        var b = this.birth;
+        var fn = function() {
+          return new Date().getFullYear() - this.birth;  //this undefined
+        }
+        return fn();
+    }
+};
+obj.getAge();
+
+/*******************************************************************************/
+
+var obj = {
+    birth : 1990,
+    getAge : function() {
+        var b = this.birth;
+        var fn = () => new Date().getFullYear() - this.birth;
+        return fn();
+    }
+};
+obj.getAge();
+
+/*******************************************************************************/
+
+var obj = {
+  birth: 1990,
+  getAge: function(year) {
+    var b = this.birth;
+    var fn = (y) => y - this.birth;
+    return fn.call({birth:2005}, year);
+  }
+}
+obj.getAge(2010);
