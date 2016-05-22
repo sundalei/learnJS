17d2a61cac0bead4e565475d275b8635d144fbf1
sundalei <sundalei2011@163.com>
2016-05-22 20:19:55 +0800

    201605222019

---
+++ browser/updateDOMTest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var js = document.getElementById('test-p');
+js.innerText = '<script>alert("Hi")</script>';
+
+js.style.color = '#ff0000';
+js.style.fontSize = '20px';
+js.style.paddingTop = '2em';
+js.style.fontWeight = 'bold';
