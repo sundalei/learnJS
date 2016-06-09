9b9e0a46e12bc82a64b0a52faba74bb6952880f0
sundalei <sundalei2011@163.com>
2016-06-09 22:54:00 +0800

    201606092253

--- node/test.js
+++ node/test.js
@@ -1,15 +1,7 @@
 'use strict';
 
+// process.nextTick()将在下一轮事件循环中调用:
 process.nextTick(function() {
   console.log('nextTick callback');
-  if(typeof(global) === 'undefined') {
-    console.log('browser');
-  } else {
-    console.log(typeof(global) + ' node.js');
-  }
 });
-process.on('exit', function(code) {
-  console.log('about to exit with code: ' + code);
-});
-console.log('nextTick was set');
-console.log('nextTick was set');
+console.log('nextTick was set!');
---
+++ node/test4.js
@@ -0,0 +1,6 @@
+'use strict';
+
+$.getJSON('http://example.com/ajax', function(data) {
+  console.log('IO结果返回后执行...');
+});
+console.log('不等待IO结果直接执行后续代码...');
