2239c05efa0b2ecf097cfb0a6a79d13c6a218908
sundalei <sundalei2011@163.com>
2016-06-12 14:49:43 +0800

    201606121449

---
+++ node/cryptoTest4.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const crypto = require('crypto');
+
+// xiaoming's key:
+var ming = crypto.createDiffieHellman(512);
+var ming_keys = ming.generateKeys();
+
+var prime = ming.getPrime();
+var generator = ming.getGenerator();
+
+console.log('Prime: ' + prime.toString('hex'));
+console.log('Generator: ' + generator.toString('hex'));
+
+// xiaohong's keys:
+var hong = crypto.createDiffieHellman(prime, generator);
+var hong_keys = hong.generateKeys();
+
+// exchange and generate secret:
+var ming_secret = ming.computeSecret(hong_keys);
+var hong_secret = hong.computeSecret(ming_keys);
+
+// print secret:
+console.log('Secret of Xiao Ming: ' + ming_secret.toString('hex'));
+console.log('Secret of Xiao Hong: ' + hong_secret.toString('hex'));
