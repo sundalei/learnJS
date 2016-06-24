f0192fa6f587af253263f6f45143c71b1541e930
sundalei <sundalei2011@163.com>
2016-06-24 16:45:26 +0800

    201606241645

--- common/stringTest.js
+++ common/stringTest.js
@@ -1,11 +1,12 @@
-//alert('I\'m \"OK\"!');
-//alert('\u4e2d\u6587');
+alert('I\'m \"OK\"!');
+alert('\x41');
+alert('\u4e2d\u6587');
 
 var s = "Hello, world!";
 s.length;
-//alert(s.toUpperCase());
-//alert(s.toLowerCase());
-//alert(s.indexOf('world'));
-//alert(s.indexOf('World'));
+alert(s.toUpperCase());
+alert(s.toLowerCase());
+alert(s.indexOf('world'));
+alert(s.indexOf('World'));
 alert(s.substring(0, 5));
 alert(s.substring(7));
