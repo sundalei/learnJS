7f49bd3bf3cccac970a5af0a9c19d89dfb68d77f
sundalei <sundalei2011@163.com>
2016-06-26 21:18:51 +0800

    201606262118

--- common/iterableTest.js
+++ common/iterableTest.js
@@ -2,24 +2,27 @@ var a = ['A', 'B', 'C'];
 var s = new Set(['A', 'B', 'C']);
 var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
 
-for(var x of a) {
+for(var x of a) { // 遍历Array
   console.log(x);
 }
-for(var x of s) {
+for(var x of s) { // 遍历Set
   console.log(x);
 }
-for(var x of m) {
+for(var x of m) { // 遍历Map
   console.log(x[0] + '=' + x[1]);
 }
 
 var a = ['A', 'B', 'C'];
 a.name = 'Hello';
 for(var x in a) {
-  console.log(x);
+  console.log(x); // '0', '1', '2', 'name'
 }
 
 var a = ['A', 'B', 'C'];
 a.forEach(function(element, index, array) {
+  // element: 指向当前元素的值
+  // index: 指向当前索引
+  // array: 指向Array对象本身
   alert(element);
 });
 
