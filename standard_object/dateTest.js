a55fa3d38a6e75106a27699d3b9e79e831b58156
sundalei <sundalei2011@163.com>
2016-07-27 20:52:33 +0800

    201607272052

--- standard_object/dateTest.js
+++ standard_object/dateTest.js
@@ -1,27 +1,27 @@
 var now = new Date();
-now;
-now.getFullYear();
-now.getMonth();
-now.getDate();
-now.getDay();
-now.getHours();
-now.getMinutes();
-now.getSeconds();
-now.getMilliseconds();
-now.getTime();
+now; // Wed Jul 27 2016 19:38:40 GMT+0800 (CST)
+now.getFullYear(); // 2016, 年份
+now.getMonth(); // 6, 月份，注意月份范围是0~11，6表示七月
+now.getDate(); // 27, 表示27号
+now.getDay(); // 3, 表示星期三
+now.getHours(); // 19, 24小时制
+now.getMinutes(); // 38, 分钟
+now.getSeconds(); // 40, 秒
+now.getMilliseconds(); // 168, 毫秒数
+now.getTime(); // 1469619520168, 以number形式表示的时间戳
 
 var d = new Date(2015, 5, 19, 20, 15, 30, 123);
-d;
+d; // Fri Jun 19 2015 20:15:30 GMT+0800 (CST)
 
 var d = Date.parse('2015-06-24T19:49:22.875+08:00');
-d;
+d; // 1435146562875
 var date = new Date(d);
-date;
+date; // Wed Jun 24 2015 19:49:22 GMT+0800 (CST)
 
 
 var d = new Date();
-d.toLocaleString();
-d.toUTCString();
+d.toLocaleString(); // '2016/7/27 下午8:39:50'，本地时间（北京时区+8:00），显示的字符串与操作系统设定的格式有关
+d.toUTCString(); // 'Wed, 27 Jul 2016 12:39:50 GMT', UTC时间，与本地时间相差8小时
 
 if (Date.now) {
   alert('Date.now');
