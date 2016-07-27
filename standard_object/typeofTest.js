259920c76f50c2fb384921625ab2c2df31401234
sundalei <sundalei2011@163.com>
2016-07-28 01:30:00 +0800

    201607271728

--- standard_object/typeofTest.js
+++ standard_object/typeofTest.js
@@ -10,9 +10,9 @@ typeof {}; // 'object'
 
 /*******************************************************************************/
 
-var n = new Number(123);
-var b = new Boolean(true);
-var s = new String('str');
+var n = new Number(123); // 123,生成了新的包装类型
+var b = new Boolean(true); // true,生成了新的包装类型
+var s = new String('str'); // 'str',生成了新的包装类型
 
 typeof new Number(123); // 'object'
 new Number(123) === 123; // false
@@ -25,14 +25,14 @@ new String('str') === 'str'; //false
 
 /*******************************************************************************/
 
-var n = Number('123');
-typeof n;
+var n = Number('123'); // 123，相当于parseInt()或parseFloat()
+typeof n; // 'number'
 
-var b = Boolean('true');
-typeof b;
+var b = Boolean('true'); // true
+typeof b; // 'boolean'
 
-var b2 = Boolean('false'); //true
-var b3 = Boolean('');
+var b2 = Boolean('false'); // true! 'false'字符串转换结果为true！因为它是非空字符串！
+var b3 = Boolean(''); // false
 
-var s = String(123.45);
-typeof s;
+var s = String(123.45); // '123.45'
+typeof s; // 'string'
