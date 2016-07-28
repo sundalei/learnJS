7f33d06a1f23cfd227b3a4b77905cc470ef34a55
sundalei <sundalei2011@163.com>
2016-07-28 17:49:29 +0800

    201607281749

--- oop/prototypeTest.js
+++ oop/prototypeTest.js
@@ -15,32 +15,43 @@ var Student = {
 };
 
 var xiaoming = {
-  name: 'xiaoming'
+  name: '小明'
 };
 
 xiaoming.__proto__ = Student;
-xiaoming.name;
-xiaoming.run();
+xiaoming.name; // 小明
+xiaoming.run(); // 小明 is running...
 
 /*******************************************************************************/
 
 var Bird = {
     fly: function() {
-        alert(this.name + ' is flying...');
+        console.log(this.name + ' is flying...');
     }
 };
 
 xiaoming.__proto__ = Bird;
-xiaoming.fly();
+xiaoming.fly(); // 小明 is flying...
 
 /*******************************************************************************/
 
+// 原型对象:
+var Student = {
+    name: 'Robot',
+    height: 1.2,
+    run: function () {
+        console.log(this.name + ' is running...');
+    }
+}
+
 function createStudent(name) {
+    // 基于Student原型创建一个新对象:
     var s = Object.create(Student);
+    // 初始化新对象:
     s.name = name;
     return s;
 }
 
-var xiaoming = createStudent('xiaoming');
-xiaoming.run();
-xiaoming.__proto__ === Student;
+var xiaoming = createStudent('小明');
+xiaoming.run(); // 小明 is running...
+xiaoming.__proto__ === Student; // true
